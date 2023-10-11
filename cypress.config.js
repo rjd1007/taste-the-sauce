12e8cc5e604a89f44dfa8f55d0d88584c21d3421
Rick Devaney <myjunkemailaccount123@yahoo.com>
2023-10-11 03:57:06 -0400

    lesson 18

--- cypress.config.js
+++ cypress.config.js
@@ -6,11 +6,25 @@ module.exports = defineConfig({
     // baseUrl, etc
     baseUrl: 'http://localhost:3000',
     supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
-      registerDataSession(on, config)
-      return config
+    env: {
+      users: {
+        standard: {
+          username: 'standard_user',
+          password: 'secret_sauce',
+        },
+        lockedOut: {
+          username: 'locked_out_user',
+          password: 'secret_sauce',
+        },
+        problem: {
+          username: 'problem_user',
+          password: 'secret_sauce',
+        },
+        glitch: {
+          username: 'performance_glitch_user',
+          password: 'secret_sauce',
+        },
+      },
     },
-  }
+  },
 })
